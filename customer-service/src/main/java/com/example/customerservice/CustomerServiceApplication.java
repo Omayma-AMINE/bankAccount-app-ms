55f05526c8cf51a0123317eefa4d2f7a6522663a
unknown <o_amine@etu.enset-media.ac.ma>
2023-11-25 12:20:38 +0100

    Commit

--- customer-service/src/main/java/com/example/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/example/customerservice/CustomerServiceApplication.java
@@ -1,16 +1,19 @@
 package com.example.customerservice;
 
+import com.example.customerservice.config.GlobalConfig;
 import com.example.customerservice.entities.Customer;
 import com.example.customerservice.repository.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 import java.util.UUID;
 import java.util.stream.Stream;
 
 @SpringBootApplication
+@EnableConfigurationProperties(GlobalConfig.class)
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
